6b119a3babc60a5f870718cdf134d97f6ebe1bb4
Michael Gutierrez <findmagpie@gmail.com>
2024-03-31 20:02:48 -0700

    add publishpress-checklists

---
+++ publishpress-checklists/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5735442995d0e54687369f867923b7bdea5e5892',
+        'name' => 'publishpress/publishpress-checklists',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'publishpress/publishpress-checklists' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5735442995d0e54687369f867923b7bdea5e5892',
+            'dev_requirement' => false,
+        ),
+    ),
+);
